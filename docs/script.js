834562bf0e23e8ea27d2affa1547154a04964ef9
dulsara-pieris <dulsara.synapsnex@gmail.com>
2026-08-20 20:39:47 +0530

    almost done just the team

--- docs/script.js
+++ docs/script.js
@@ -13,3 +13,43 @@ const moments = new Swiper(".moments-stack", {
                 slideShadows: false,
         },
 });
+
+
+// CONTACT PAGE - email
+
+const form = document.getElementById('form');
+const submitBtn = form.querySelector('button[type="submit"]');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    formData.append("access_key", "18f1d405-e2ea-4a90-b592-ecf139f4993a");
+
+    const originalText = submitBtn.textContent;
+
+    submitBtn.textContent = "Sending...";
+    submitBtn.disabled = true;
+
+    try {
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            alert("Success! Your message has been sent.");
+            form.reset();
+        } else {
+            alert("Error: " + data.message);
+        }
+
+    } catch (error) {
+        alert("Something went wrong. Please try again.");
+    } finally {
+        submitBtn.textContent = originalText;
+        submitBtn.disabled = false;
+    }
+});
